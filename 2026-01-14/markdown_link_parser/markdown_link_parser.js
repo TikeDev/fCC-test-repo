c790d6fce503464d4df38e07bf38005414f629a8
KM <12630355+TikeDev@users.noreply.github.com>
2026-02-02 22:15:32 -0500

    Add solution - freeCodeCamp Daily Code Challenge

---
+++ 2026-01-14/markdown_link_parser/markdown_link_parser.js
@@ -0,0 +1,24 @@
+function parseLink
+(markdown) {
+  let split = 
+markdown.split("](");
+  let text = split[0].
+slice(1, split[0].
+length);
+  let url = split[1].
+slice(0, split[1].
+length-1)
+  return `<a href="$
+{url}">${text}</a>`;
+}
+/*
+Input: markdown 
+format image link
+"[link_text](link_url)
+"
+Output: anchor tag w 
+href and content
+<a 
+href="link">Content </
+a>
+"[freeCodeCamp]
